3fd15146296ff4559f1418f1066509afa4e6e6a0
Wayface <wayfacee@gmail.com>
2024-07-24 18:05:15 +0600

    fixed problems

--- src/features/AuthByUsername/model/slice/loginSlice.test.ts
+++ src/features/AuthByUsername/model/slice/loginSlice.test.ts
@@ -9,7 +9,7 @@ describe('loginSlice', () => {
     expect(loginReducer(
       state as LoginSchema,
       loginActions.setUsername('123123')
-    )).toBe({ username: '123123' });
+    )).toEqual({ username: '123123' });
   });
 
   test('test set username', () => {
@@ -18,7 +18,7 @@ describe('loginSlice', () => {
     expect(loginReducer(
       state as LoginSchema,
       loginActions.setPassword('123123')
-    )).toBe({ password: '123123' });
+    )).toEqual({ password: '123123' });
   });
 
   // лоадинг и еррор, лучше не над, в слайсе это избыточно
