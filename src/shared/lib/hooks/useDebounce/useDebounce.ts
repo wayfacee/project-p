d173f6c5f4e76e0ca9d3d9d433b4d13f9c9bd9ca
Wayface <wayfacee@gmail.com>
2024-08-12 16:15:04 +0600

    add js doc

--- src/shared/lib/hooks/useDebounce/useDebounce.ts
+++ src/shared/lib/hooks/useDebounce/useDebounce.ts
@@ -1,10 +1,15 @@
+import { MutableRefObject, useCallback, useRef } from "react";
 
 // отменять пред. событъе в течении какого-то времени
 // до тех пор пока что то ввдоим в инпут, каллбэк
 // не будет выз., когда пройдет делай, тогда будет
 // вызван каллбэк, а все пред вызовы. отменены
-import { MutableRefObject, useCallback, useRef } from "react";
 
+/**
+ * Хук, который позволяет отменять предыдущий вызов функции пока не истечет delay
+ * @param callback
+ * @param delay - задержка в мс
+ */
 export function useDebounce(callback: (...args: any[]) => void, delay: number) {
   const timer = useRef() as MutableRefObject<any>; 
   
--- src/shared/lib/hooks/useModal/useModal.tsx
+++ src/shared/lib/hooks/useModal/useModal.tsx
@@ -6,6 +6,12 @@ interface UseModalProps {
   animationDelay: number;
 }
 
+/**
+ * Переиспользуемый хук для модальных компонентов (drawer/modal)
+ * @param animationDelay
+ * @param isOpen
+ * @param onClose
+ */
 export function useModal(props: UseModalProps) {
   const {
     isOpen,
