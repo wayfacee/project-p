b48e3eba567daeab989a15fd1cd9f2c1234f6363
Wayface <wayfacee@gmail.com>
2024-08-12 16:21:38 +0600

    fix unit with aliases, fix storybook after refactoring, update screens

--- config/storybook/preview.ts
+++ config/storybook/preview.ts
@@ -1,9 +1,9 @@
 import type { Preview } from "@storybook/react";
-import { Theme } from "@/app/providers/ThemeProvider";
 import { RouterDecorator } from "@/shared/config/storybook/RouterDecorator/RouterDecorator";
 import { StyleDecorator } from "@/shared/config/storybook/StyleDecorator/StyleDecorator";
 import { SuspenseDecorator } from "@/shared/config/storybook/SuspenseDecorator/SuspenseDecorator";
 import { ThemeDecorator } from "@/shared/config/storybook/ThemeDecorator/ThemeDecorator";
+import { Theme } from "@/shared/const/theme";
 
 const preview: Preview = {
   parameters: {
@@ -13,6 +13,8 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
+    // без педддингов:
+    layout: 'fullscreen'
   },
   decorators: [
     // обретка, типо импорт стайлов, чтоб кд раз не импортить, порядок не важен
