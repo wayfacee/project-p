346c03e7145300c2d8ee4f4d36ae49a582f48eff
Wayface <wayfacee@gmail.com>
2024-08-14 21:04:42 +0600

    add argument for buildSelector

--- src/shared/store/buildSelector.ts
+++ src/shared/store/buildSelector.ts
@@ -6,13 +6,24 @@ import { useSelector } from 'react-redux';
  */
 
 // то чо возв., генриком будет подхв. и автоматом. типиз.
-type Selector<T> = (state: StateSchema) => T;
-type Result<T> = [() => T, Selector<T>];
+// any[] - mb string, number etc.
+type Selector<T, Args extends any[]> = (
+  state: StateSchema,
+  ...args: Args[]
+) => T;
+// чтоб автомкомплит подхватывал + резалт затипизирован
+// в хуке не указаны аргы, чтобы было все по красоте
+// надо исп. генерики, чтобы тс подхватывал типы которые передаем 10стр.
+type Hook<T, Args extends any[]> = (...args: Args[]) => T;
+// Hook<T, Args> - то что принимает функц.
+type Result<T, Args extends any[]> = [Hook<T, Args>, Selector<T, Args>];
 
-export function buildSelector<T>(selector: Selector<T>): Result<T> {
-  const useSelectorHook = () => {
-    return useSelector(selector);
-  };
+export function buildSelector<T, Args extends any[]>(
+  selector: Selector<T, Args>,
+): Result<T, Args> {
+  const useSelectorHook: Hook<T, Args> = (...args: Args[]) => {
+    return useSelector((state: StateSchema) => selector(state, ...args));
+  }; 
 
   // 1) hook - внутри комп. (можем дать любое назв.)
   // 2) сам селектор (use in async thunk, helpers etc.)
